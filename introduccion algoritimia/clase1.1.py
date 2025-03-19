ad4800e07ab1dea078b2ceae44b44b999c359a04
Emanuel Strah <manustrah@gmail.com>
2025-03-19 13:13:56 -0300

    clase 1 python

---
+++ introduccion algoritimia/clase1.1.py
@@ -0,0 +1,8 @@
+hombres=int(input("cantidad de hombres"))
+mujeres=int(input("cantidad de mujeres"))
+total=mujeres+hombres
+print("cantidad total de personas en el curso",total)
+porcentaje_hombres= hombres / total *100
+porcentaje_mujeres= mujeres/total*100
+print("porcentaje total de hombres",porcentaje_hombres)
+print("porcentaje total de mujeres",porcentaje_mujeres)
---
+++ introduccion algoritimia/clase1.py
@@ -0,0 +1,9 @@
+parcial1 = int(input("ingrese el resultado de su primer parcial: "))
+parcial2 = int(input("ingrese el resultado de su segundo parcial: "))
+
+promedio = (parcial1 + parcial2) / 2
+
+print("el promedio de sus dos parciales es: ",promedio)
+
+
+
