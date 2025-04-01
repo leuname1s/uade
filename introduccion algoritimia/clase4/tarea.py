3cb5218e74c40a5536c4e64eeb5143b7fcd5e41c
Emanuel Strah <manustrah@gmail.com>
2025-04-01 17:03:34 -0300

    tarea

---
+++ introduccion algoritimia/clase4/tarea.py
@@ -0,0 +1,17 @@
+import random
+cNiven = 0
+c = 0
+while c < 100:
+    numero = random.randint(100,999)
+    sumaDigitos = 0
+    numeroAux = numero
+    while  numeroAux > 0:
+        digito = numeroAux % 10
+        sumaDigitos += digito
+        numeroAux = numeroAux // 10
+    if numero % sumaDigitos == 0:
+        print(numero)
+        cNiven += 1
+    c += 1
+    
+print("aparecieron ", cNiven," numero de niven")
