4d32efd31e392d968a88b470dc5d6b55fc5949d1
Emanuel Strah <manustrah@gmail.com>
2025-04-15 13:51:44 -0300

    ejercicio2 algoritimia

---
+++ introduccion algoritimia/clase6/ejercicio1.py
@@ -0,0 +1,4 @@
+resp = 0
+
+while resp != -1:
+    resp = int(input("ingrese una opcion: 1 - perro / 2 - gato / 3 - conejo / -1 - salir: "))
---
+++ introduccion algoritimia/clase6/ejercicio2.py
@@ -0,0 +1,31 @@
+resp = 0
+notaParcial1 = 0
+notaParcial2 = 0
+
+cantPromocionados = 0
+cantRecuperatorio = 0
+cantFinal = 0
+cantAlumnos = 0
+while resp != -1:
+    resp = int(input("ingrese su numero de libreta(1000000 y 2000000): "))
+    if resp >= 1000000 and resp <= 2000000:
+        cantAlumnos += 1
+        notaParcial1 = int(input("ingrese la nota del primer parcial: "))
+        notaParcial2 = int(input("ingrese la nota del segundo parcial: "))
+        if notaParcial1 < 0 or notaParcial1 > 10 or notaParcial2 < 0 or notaParcial2 > 10:
+            print("Error, las notas deben estar entre 0 y 10")
+        else:
+            if notaParcial1 > 7 and notaParcial2 > 7:
+                cantPromocionados += 1
+            elif notaParcial1 >= 4 and notaParcial2 >= 4:
+                cantFinal += 1
+            elif notaParcial1 < 4 or notaParcial2 < 4:
+                cantRecuperatorio += 1
+    else:
+        print("Error, el numero de libreta no es valido")
+        
+        
+print("Cantidad de alumnos promocionados(más de ocho en ambos parciales): ", cantPromocionados)
+print("Cantidad de alumnos que deben rendir al menos un recuperatorio: ", cantRecuperatorio)
+print("porcentaje de alumnos que rinden final: ", (cantFinal/cantAlumnos)*100, "%")
+                
