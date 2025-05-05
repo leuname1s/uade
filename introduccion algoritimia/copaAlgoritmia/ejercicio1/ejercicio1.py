7e1a5deae6995584c96950c2be0f385f0c0003bf
Emanuel Strah <manustrah@gmail.com>
2025-05-05 15:04:15 -0300

    copa v1

--- introduccion algoritimia/copaAlgoritmia/ejercicio1/ejercicio1.py
+++ introduccion algoritimia/copaAlgoritmia/ejercicio1/ejercicio1.py
@@ -1,12 +1,31 @@
-import csv
+import csv  #libreria para archivos .csv
 
-with open("preguntas.csv", encoding="utf-8") as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+preguntasRespuestas = dict()    #un diccionario para tener en memoria
 
-respuesta = input("""ingrese una pregunta (ingrese "salir" para salir): """)
+with open("preguntas.csv","r", newline="",encoding='utf-8') as csvfile:
+    csvReader = csv.reader(csvfile)
+    next(csvReader) #saltear la primera fila que es el encabezado
+    for row in csvReader:
+        preguntasRespuestas[row[0]] = row[1]    #se registran las preguntas y respuestas del archivo "preguntas.csv" en el diccionario
+        
+        
+r = input("""-> Ingrese una pregunta (ingrese "salir" para salir): """)
 
+while r != "salir":     #bucle principal
+    if r in preguntasRespuestas: #si la pregunta esta registrada
+        print("-> ",preguntasRespuestas[r])
+    else:   #si la pregunta NO esta registrada
+        r2 = input("-> No conozco esa pregunta ¿queres ingresar una respuesta a esa pregunta?: ")
+        if r2 in {"si","Si","sí","Sí","s","y"}: #si se quiere ingresar una respuesta para la pregunta no resgitrada
+            
+            respuestaPregunta = input("-> ¿Cual sería la respuesta esperada para esa pregunta?: ")
+            with open("preguntas.csv","a", newline="",encoding='utf-8') as csvfile: #registra la respuesta a la pregunta
+                csvWriter = csv.writer(csvfile)
+                csvWriter.writerow([r,respuestaPregunta])
+            preguntasRespuestas[r] = respuestaPregunta
+            print("-> respuesta registrada, ¡gracias!")
+    
+    r = input("""-> Ingrese una pregunta (ingrese "salir" para salir): """) #repite el bucle
 
-while respuesta != "salir":
-    pass
+print("-> ¡Chau!")
+    
